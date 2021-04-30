59245515a09904d58d45870e9e69cbf56dfc4778
gledson-ss <glegogle84@gmail.com>
2021-04-30 06:53:19 -0300

    :collision: update route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,23 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Dashboard.vue'
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import Dashboard from "../views/Dashboard.vue";
+import Mission from "../views/Mission.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Dashboard",
+    component: Dashboard
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: "/mission/:id",
+    name: "Mission",
+    component: Mission
   }
-]
+];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
 
-export default router
+export default router;
