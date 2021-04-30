06221ca486be6f46bde701efb26641d0422cda45
gledson-ss <glegogle84@gmail.com>
2021-04-30 06:37:29 -0300

    :star: update dashboard route

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,20 +1,24 @@
 <template>
   <div class="home">
     <SectionDescription description="Last Launches"/>
+    <MenuLauches />
   </div> 
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import SectionDescription from '../components/SectionDescription.vue'
+import MenuLauches from '../components/MenuLauches.vue'
 
 
 export default defineComponent({
   components:{
-    SectionDescription
+    SectionDescription,
+    MenuLauches
   }
 })
 </script>
 
 <style>
+ 
 </style>
