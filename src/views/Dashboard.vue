5466e82b7412b0475d55861f04eabe329fb5422a
gledson-ss <glegogle84@gmail.com>
2021-04-30 06:32:42 -0300

    :star: changed view

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <SectionDescription description="Last Launches"/>
+  </div> 
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import SectionDescription from '../components/SectionDescription.vue'
+
+
+export default defineComponent({
+  components:{
+    SectionDescription
+  }
+})
+</script>
+
+<style>
+</style>
