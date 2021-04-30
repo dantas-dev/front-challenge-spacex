f0445509f7b13590b137f754a3e28418ae5b1ed4
gledson-ss <glegogle84@gmail.com>
2021-04-30 06:51:16 -0300

    :star: update route

---
+++ src/views/Mission.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="about">
+    <SectionDescription description="Mission"/>
+    <router-link to="/">
+      <Button text="Back"/>
+    </router-link>
+    
+    <MenuMission />
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+import SectionDescription from '../components/SectionDescription.vue'
+import MenuMission from '../components/MenuMission.vue'
+import Button from '../components/Button.vue'
+export default defineComponent({
+  components:{
+    SectionDescription,
+    MenuMission,
+    Button
+  }
+})
+</script>
+
+<style>
+ 
+</style>
