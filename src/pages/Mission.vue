236df68caab6eec9976a7f1a8c6bc80aeb0508cd
jonathanccardoso <jonathan99moura@hotmail.com>
2021-05-14 23:36:58 -0300

    Fixme: update page mission with infos from props apollo client

--- src/pages/Mission.vue
+++ src/pages/Mission.vue
@@ -1,29 +1,112 @@
 <template>
   <main>
     <section class="container">
-      <img src="https://i.imgur.com/NHsx95l.jpg" alt="Image" />
-      <h1>Mission name</h1>
-      <p>
-        Lorem Ipsum é simplesmente uma simulação de texto da indústria
-        tipográfica e de impressos, e vem sendo utilizado desde o século XVI,
-        quando um impressor desconhecido pegou uma bandeja de tipos e os
-        embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum
-        sobreviveu não só a cinco séculos, como também ao salto para a
-        editoração eletrônica, permanecendo essencialmente inalterado. Se
-        popularizou na década de 60, quando a Letraset lançou decalques contendo
-        passagens de Lorem Ipsum, e mais recentemente quando passou a ser
-        integrado a softwares de editoração eletrônica como Aldus PageMaker.
-      </p>
-      <a href="#" target="_blank" rel="noopener noreferrer">See more</a>
+      <ApolloQuery
+        :query="
+          (gql) => gql`
+            query LaunchQuery($id: ID!) {
+              launch(id: $id) {
+                mission_name
+                details
+                links {
+                  video_link
+                }
+                ships {
+                  name
+                  image
+                }
+              }
+            }
+          `
+        "
+        :variables="{ id }"
+      >
+        <template v-slot="{ result: { loading, error, data } }">
+          <!-- Loading -->
+          <div v-if="loading" class="loading apollo">Loading...</div>
+
+          <!-- Error -->
+          <div v-else-if="error" class="error apollo">An error occurred</div>
+
+          <!-- Result -->
+          <div v-else-if="data" class="container">
+            <div v-if="data.launch.ships.length >= 1">
+              <swiper class="swiper" :options="swiperOption">
+                <swiper-slide
+                  v-for="ship in data.launch.ships"
+                  :key="ship.name"
+                >
+                  <img :src="ship.image" :alt="ship.name" />
+                </swiper-slide>
+                <div class="swiper-button-prev" slot="button-prev"></div>
+                <div class="swiper-button-next" slot="button-next"></div>
+              </swiper>
+            </div>
+            <div v-else>
+              <swiper class="swiper" :options="swiperOption">
+                <swiper-slide>
+                  <span>With no images assigned to the mission.</span>
+                </swiper-slide>
+                <div class="swiper-button-prev" slot="button-prev"></div>
+                <div class="swiper-button-next" slot="button-next"></div>
+              </swiper>
+            </div>
+            <h1>{{ data.launch.mission_name }}</h1>
+            <p v-if="data.launch.details">{{ data.launch.details }}</p>
+            <p v-else>With no details assigned to the mission.</p>
+            <a
+              :href="data.launch.links.video_link"
+              target="_blank"
+              rel="noopener noreferrer"
+              >See more</a
+            >
+          </div>
+
+          <!-- No result -->
+          <div v-else class="no-result apollo">No result :(</div>
+        </template>
+      </ApolloQuery>
     </section>
   </main>
 </template>
 
 <script>
+import { Swiper, SwiperSlide, directive } from "vue-awesome-swiper";
+import "swiper/css/swiper.css";
+
 export default {
   name: "Mission",
+  data() {
+    return {
+      id: this.$route.params.id,
+      isLoading: true,
+      fullPage: true,
+      dataCarousel: [
+        '<div class="example-slide">Slide 1</div>',
+        '<div class="example-slide">Slide 2</div>',
+        '<div class="example-slide">Slide 3</div>',
+      ],
+      swiperOption: {
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      },
+    };
+  },
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  directives: {
+    swiper: directive,
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+@import "../styles/carousel.scss";
+</style>
 
 <style scoped>
 .container {
@@ -31,9 +114,10 @@ export default {
   flex-direction: column;
   width: 1140px;
   max-width: 100%;
+  padding-top: 1.5rem;
   padding-right: 15px;
   padding-left: 15px;
-  padding-bottom: 2rem;
+  padding-bottom: 1rem;
   margin-left: auto;
   margin-right: auto;
 }
@@ -47,6 +131,7 @@ img {
 h1 {
   font-size: 1.8rem;
   text-align: center;
+  padding-top: 1rem;
 }
 p {
   text-align: justify;
@@ -56,8 +141,11 @@ a {
   display: flex;
   align-self: center;
   text-align: center;
-  border: 2px solid gray;
+  justify-self: center;
+  justify-content: center;
+  border: 2px solid var(--gray-800);
   padding: 1rem;
+  transition: 0.3s;
 }
 a:hover {
   background-color: var(--gray-800);
