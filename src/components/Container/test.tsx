ffe4166d1a8ace6ee4bcae545b6986b692521e27
Aldo-1 <aldo.wanderley@souunit.com.br>
2021-05-24 20:52:24 -0300

    feat - creating Container component test

--- src/components/Container/test.tsx
+++ src/components/Container/test.tsx
@@ -3,7 +3,30 @@ import { render } from '@testing-library/react'
 import { Container } from '.'
 
 describe('<Container />', () => {
-  it('should render the heading', () => {
-    render(<Container />)
+  it('should render the container', () => {
+    const { container } = render(
+      <Container>
+        <h1>Container</h1>
+      </Container>
+    )
+    expect(container.firstChild).toHaveStyleRule('max-width', '1100px')
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c0 {
+        width: 100%;
+        max-width: 1100px;
+        margin-left: auto;
+        margin-right: auto;
+        padding-left: 1.6rem;
+        padding-right: 1.6rem;
+      }
+
+      <div
+        class="c0"
+      >
+        <h1>
+          Container
+        </h1>
+      </div>
+    `)
   })
 })
