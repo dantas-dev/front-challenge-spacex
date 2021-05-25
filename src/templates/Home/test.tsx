8c8b3d02ba091a815e09c31de6bbc134a3df1f62
Aldo-1 <aldo.wanderley@souunit.com.br>
2021-05-24 21:34:54 -0300

    feat - creating home test

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -1,9 +1,17 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import mock from './mock'
 import Home from '.'
 
+jest.mock('components/LaunchCard', () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid="Mock LaunchCard">{children}</div>
+  }
+}))
+
 describe('<Home />', () => {
-  it('should render the heading', () => {
+  it('should render the home page', () => {
     render(<Home launchesPast={mock} />)
+    expect(screen.getAllByTestId('Mock LaunchCard')).toHaveLength(10)
   })
 })
