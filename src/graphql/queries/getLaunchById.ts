e4561dc8eff03d5de3a8a559758de07bdeb59ba7
Aldo-1 <aldo.wanderley@souunit.com.br>
2021-05-24 22:13:15 -0300

    feat - creating new graphQl Querie

---
+++ src/graphql/queries/getLaunchById.ts
@@ -0,0 +1,41 @@
+const GET_LAUNCHES_BY_ID = /* GraphQL */ `
+  query GET_LAUNCHES_BY_ID($id: ID!) {
+    launchesPast(limit: 10, id: $id) {
+      mission_name
+      launch_date_local
+      launch_site {
+        site_name_long
+      }
+      links {
+        article_link
+        video_link
+      }
+      rocket {
+        rocket_name
+        first_stage {
+          cores {
+            flight
+            core {
+              reuse_count
+              status
+            }
+          }
+        }
+        second_stage {
+          payloads {
+            payload_type
+            payload_mass_kg
+            payload_mass_lbs
+          }
+        }
+      }
+      ships {
+        name
+        home_port
+        image
+      }
+    }
+  }
+`
+
+export default GET_LAUNCHES_BY_ID
