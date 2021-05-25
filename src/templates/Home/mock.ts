9324eece68c95a7553c9802eac79f558f8c8ff81
Aldo-1 <aldo.wanderley@souunit.com.br>
2021-05-24 21:34:54 -0300

    feat - creating home test

--- src/templates/Home/mock.ts
+++ src/templates/Home/mock.ts
@@ -48,10 +48,5 @@ export default [
     mission_name: 'Starlink-15 (v1.0)',
     mission_link: 'starlink-15',
     launch_date: '2020-10-24T11:31:00-04:00'
-  },
-  {
-    mission_name: 'Starlink-15 (v1.0)',
-    mission_link: 'starlink-15',
-    launch_date: '2020-10-24T11:31:00-04:00'
   }
 ]
