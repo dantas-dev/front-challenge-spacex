dd58beb0226d43e4f247befe6cbc3b1a175698fd
Aldo-1 <aldo.wanderley@souunit.com.br>
2021-05-24 23:42:25 -0300

    feat - creating test to mission details

--- src/components/MissionDetails/mock.ts
+++ src/components/MissionDetails/mock.ts
@@ -1,5 +1,5 @@
 export default {
-  mission_name: 'Starlink-15 (v1.0)',
+  mission_name: 'Starlink-15',
   details:
     'SpaceX will launch Sentinel-6 Michael Freilich into low Earth orbit for NASA, NOAA, ESA, and the European Organization for the Exploitation of Meteorological Satellites aboard a Falcon 9 from SLC-4E, Vandenberg Air Force Station. Sentinel-6(A) is an ocean observation satellite providing radar ocean surface altimetry data and also atmospheric temperature profiles as a secondary mission. The booster for this mission is will land at LZ-4.',
   ships: [
@@ -25,7 +25,7 @@ export default {
     }
   ],
   links: {
-    article_link: null,
+    article_link: 'https://youtu.be/J442-ti-Dhg',
     video_link: 'https://youtu.be/J442-ti-Dhg'
   }
 }
