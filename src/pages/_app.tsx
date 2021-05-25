6c1826a774806c5bc5390735154c2e0df0614537
Aldo-1 <aldo.wanderley@souunit.com.br>
2021-05-24 22:28:10 -0300

    feat - new icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,6 @@ function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>SpaceX - Test</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
         <meta
