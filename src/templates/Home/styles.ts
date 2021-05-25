0505cfc50bfef7c28ac850eb94ac3b1e71e98005
Aldo-1 <aldo.wanderley@souunit.com.br>
2021-05-24 21:29:44 -0300

    feat - creating mock and adjusting some style in Home

--- src/templates/Home/styles.ts
+++ src/templates/Home/styles.ts
@@ -4,4 +4,15 @@ export const Wrapper = styled.main`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  height: 100%;
+  padding: 1.6rem 0;
+  a {
+    &:not(:first-child) {
+      margin-top: 1.6rem;
+    }
+  }
+`
+
+export const Title = styled.h1`
+  color: #273944;
 `
