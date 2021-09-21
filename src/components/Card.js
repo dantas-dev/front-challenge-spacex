509381c759a8d9b8d5025260b3a7949cc29773b8
Gabriel Sena <gabesenacom@gmail.com>
2021-09-21 12:55:13 -0300

    Import Card.css styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { IconX } from '@tabler/icons'
 import LinkButton from './LinkButton'
 import Button from './Button'
+import './../styles/Card.css'
 
 const Card = ({ image, name, details, date, external_link }) => {
   const [expanded, setExpanded] = useState(false)
@@ -11,27 +12,36 @@ const Card = ({ image, name, details, date, external_link }) => {
     event.stopPropagation()
   }
 
+  function getDetails () {
+    if (expanded) return details
+    return Array.from(details)
+      .splice(0, 120)
+      .join('')
+      .concat('...')
+  }
+
   return (
     <div onClick={onClick} className={expanded ? 'card expanded' : 'card'}>
-      {image && <img src={image} alt={name} />}
-      {name && <h3>{name}</h3>}
-      {details && <p>{details}</p>}
-      {date && <small>{date}</small>}
-      {expanded && (
-        <LinkButton
-          onClick={event => event.stopPropagation()}
-          text='See more'
-          href={external_link}
-          rel='noreferrer'
-          target='_blank'
-        />
-      )}
       {expanded && (
         <Button
           icon={IconX}
           onClick={onClick}
           type='button'
-          className='close-card-button'
+          className='close-card-button danger'
+        />
+      )}
+      {image && <img className='card-image' src={image} alt={name} />}
+      {name && <h4 className='card-name'>{name}</h4>}
+      {details && <p className='card-details'>{getDetails()}</p>}
+      {date && <p className='card-date'>{date}</p>}
+      {expanded && (
+        <LinkButton
+          className='card-link-button'
+          onClick={event => event.stopPropagation()}
+          text='See more'
+          href={external_link}
+          rel='noreferrer'
+          target='_blank'
         />
       )}
     </div>
