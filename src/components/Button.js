3995b54f121d64ffe55f085b9ffa43f0d01a8302
Gabriel Sena <gabesenacom@gmail.com>
2021-09-21 09:21:15 -0300

    Set className with type of button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,9 @@
-const Button = ({ text, icon, onClick, className, type }) => {
+import './../styles/Button.css'
+import './../styles/IconButton.css'
+
+const Button = ({ text, icon, type, className, onClick }) => {
+  className = 'button'.concat(' ', className || '').concat(' ', icon && (!text || text.length === 0) ? 'icon' : '')
+
   return (
     <button type={type} className={className} onClick={onClick}>
       {icon && icon({})}
