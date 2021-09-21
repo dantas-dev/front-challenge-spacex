3d9f6cc80d8d523aad7703a0e791bb8f2c99e2af
Gabriel Sena <gabesenacom@gmail.com>
2021-09-21 00:28:40 -0300

    Add query_data module
    This module will control the graphql api

---
+++ src/api/query_data.js
@@ -0,0 +1,12 @@
+async function fetchLaunches(query) {
+  //will find data and return response json
+}
+
+
+function getLaunches(limit) {
+  //will create a query with a limit and convert the response data to this object:
+  // {image, name, details, date, ref_link}
+  // the result is a array with this objects
+}
+
+export {fetchLaunches, getLaunches}
