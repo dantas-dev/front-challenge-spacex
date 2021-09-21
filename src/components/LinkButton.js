d47d2980274e73e5504ee223fbb1153bc08ca984
Gabriel Sena <gabesenacom@gmail.com>
2021-09-21 12:56:39 -0300

    Apply Prettier

--- src/components/LinkButton.js
+++ src/components/LinkButton.js
@@ -1,6 +1,12 @@
 const LinkButton = ({ text, className, href, rel, target, onClick }) => {
   return (
-    <a className={'button '+(className || '')} onClick={onClick} href={href} rel={rel} target={target}>
+    <a
+      className={'button ' + (className || '')}
+      onClick={onClick}
+      href={href}
+      rel={rel}
+      target={target}
+    >
       {text}
     </a>
   )
