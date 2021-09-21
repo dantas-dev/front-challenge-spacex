941893a4505ed04f5796d5ce1056152789814f54
Gabriel Sena <gabesenacom@gmail.com>
2021-09-21 01:16:41 -0300

    Apply Prettier

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,24 +1,22 @@
 import uniqid from 'uniqid'
 import Card from './Card'
 
-const CardList = ({launches}) => {
-
-  let cards = launches.map((launch) => {
-    return <Card 
-    image={launch.image} 
-    name={launch.name} 
-    details={launch.details} 
-    date={launch.date} 
-    external_link={launch.ref_link}
-    key={uniqid()}
-    />
+const CardList = ({ launches }) => {
+  
+  let cards = launches.map(launch => {
+    return (
+      <Card
+        image={launch.image}
+        name={launch.name}
+        details={launch.details}
+        date={launch.date}
+        external_link={launch.ref_link}
+        key={uniqid()}
+      />
+    )
   })
 
-  return (
-      <ul className="card-list">
-        {cards}
-      </ul>
-    )
+  return <ul className='card-list'>{cards}</ul>
 }
 
 export default CardList
