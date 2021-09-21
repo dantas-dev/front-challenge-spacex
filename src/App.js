042b6aed124545996524e48c74791cfc8dcb8136
Gabriel Sena <gabesenacom@gmail.com>
2021-09-20 22:56:36 -0300

    Add Header component and latestLaunches state on App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import Header from './components/Header'
+import { useState, useEffect } from 'react'
+
 const App = () => {
-  return <h1>Hello World!</h1>
+
+  const [latestLaunches, setLatestLaunches] = useState(10)
+
+  useEffect(() => {
+    //call query api to update card list
+  }, [latestLaunches])
+  
+  const onHeaderSubmit = (event) => {
+    event.preventDefault()
+    const target = event.target.querySelector('.search-latest-launches')
+    if(target) {
+      setLatestLaunches(() => parseInt(target.value))
+    }
+  }
+
+  return (
+    <main>
+      <Header onSubmit={onHeaderSubmit}/>
+    </main>
+    )
 }
 
-export default App;
+export default App
