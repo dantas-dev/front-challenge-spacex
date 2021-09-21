8c49bf3ed48346e557fe1da8fd785dc193cbf247
Gabriel Sena <gabesenacom@gmail.com>
2021-09-21 00:15:09 -0300

    Add Card on App; Apply Prettier

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import Header from './components/Header'
+import CardList from './components/CardList'
+
 import { useState, useEffect } from 'react'
 
 const App = () => {
 
   const [latestLaunches, setLatestLaunches] = useState(10)
+  const [launches, setLaunches] = useState([])
 
   useEffect(() => {
     //call query api to update card list
   }, [latestLaunches])
-  
-  const onHeaderSubmit = (event) => {
+
+  const onHeaderSubmit = event => {
     event.preventDefault()
     const target = event.target.querySelector('.search-latest-launches')
-    if(target) {
+    if (target) {
       setLatestLaunches(() => parseInt(target.value))
     }
   }
-
+  
   return (
     <main>
-      <Header onSubmit={onHeaderSubmit}/>
+      <Header onSubmit={onHeaderSubmit} />
+      <CardList launches={launches}/>
     </main>
-    )
+  )
 }
 
 export default App
