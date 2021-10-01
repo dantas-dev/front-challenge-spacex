6fe2f71efed54e071b46c2a922e7041fb50a9f90
Adryan Rosa <adryanrosa374@gmail.com>
2021-10-01 17:14:29 -0300

    Adiciona estilização inicial Mission details

--- src/pages/Mission/index.jsx
+++ src/pages/Mission/index.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { useParams } from 'react-router';
 import { request, gql } from 'graphql-request';
 
+import './index.scss';
+
 function Mission() {
   const { missionId } = useParams();
 
@@ -38,19 +40,25 @@ function Mission() {
   if (loading) return 'Loading';
 
   return (
-    <main>
-      <img src={ mission.links.flickr_images[0] } alt={ mission.mission_name } />
+    <main className="mission">
+      <section className="mission__details">
+        <div className="mission__details__image-container">
+          <img src={ mission.links.flickr_images[0] } alt={ mission.mission_name } />
+        </div>
 
-      <h2>{mission.mission_name}</h2>
-      <p>{mission.details}</p>
+        <div className="mission__details__text-container">
+          <h2>{mission.mission_name}</h2>
+          <p>{mission.details}</p>
 
-      <a
-        href={ mission.links.article_link }
-        target="_blank"
-        rel="noreferrer"
-      >
-        See more
-      </a>
+          <a
+            href={ mission.links.article_link }
+            target="_blank"
+            rel="noreferrer"
+          >
+            See more
+          </a>
+        </div>
+      </section>
     </main>
   );
 }
