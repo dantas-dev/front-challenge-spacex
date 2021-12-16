5fcd4ad4184a8915abb9b88b24057b20648f80b9
Victor Carvalho <victor.carvalho@simoninisoftware.com.br>
2021-12-16 19:11:51 -0300

    initial commit

---
+++ SpacexLaunches/src/app/mission/mission.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-mission',
+  templateUrl: './mission.component.html',
+  styleUrls: ['./mission.component.scss']
+})
+export class MissionComponent implements OnInit {
+  
+  public mission : any = {
+    'mission_name': String,
+      'details': String,
+      'launch_date_local': new Date(),
+      'links': Object,
+      'ships': Object
+  };
+  constructor(private activatedRoute: ActivatedRoute) {}
+  
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe((res) => {
+      this.mission = JSON.parse(res["data"]);
+      console.log('na mission', this.mission)
+  });
+  }
+
+}
