c527640bab94458fd85946fd55565f0d91642cef
Victor Carvalho <victor.carvalho@simoninisoftware.com.br>
2021-12-17 15:30:48 -0300

    added instructions

---
+++ SpacexLaunches/INSTRUCTIONS.md
@@ -0,0 +1,17 @@
+# SpacexLaunches
+
+- Home/Dashboard screen with the latest SpaceX missions.
+![alt text](/src/assets/dashboard.png)
+
+- Mission screen with the mission details, external links and a back button.
+![alt text](/src/assets/mission.png)
+
+- Setup instructions
+
+- How did you decide which technologies to use as part of your solution
+- Are there any improvements you could make to your submission
+- What would you do differently if you were allocated more time
+
+## Details
+Use the [SpaceX Graphql API](https://api.spacex.land/graphql/) and create a project based on this [wireframe](https://bit.ly/2SwvPSP). Use your creativity to choose colors etc...
+
--- SpacexLaunches/src/app/app.component.spec.ts
+++ SpacexLaunches/src/app/app.component.spec.ts
@@ -13,29 +13,4 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   });
-
-  // it('should create the app', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app).toBeTruthy();
-  // });
-
-  // it(`should have as title 'SpacexLaunches'`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('SpacexLaunches');
-  // });
-
-  // it('should create the home', () => {
-  //   const fixture = TestBed.createComponent(HomeComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app).toBeTruthy();
-  // });
-
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('.content span')?.textContent).toContain('SpacexLaunches app is running!');
-  // });
 });
